161b160775dbbd6fc94a08bd7ee1cb926a204be8
yusaku-kanamo-25 <yuusaku.kanamo.25@dl-techx.com>
2025-12-02 17:52:43 +0900

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -30,7 +30,7 @@ new Vue({
         const numberOfParticipants = participantList.length;
         const amountPerParticipant = this.totalAmount / numberOfParticipants;
 
-        const response = await fetch('https://m3h-beerkn-functionapp.azurewebsites.net/api/savenomikai', {
+        const response = await fetch('https://nomikai-funcapp-g4hdgyachmhwfxbq.japaneast-01.azurewebsites.net/api/savenomikai', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -58,7 +58,7 @@ new Vue({
     },
     async searchNomikaiEvent() {
       try {
-        let url = 'https://m3h-beerkn-functionapp.azurewebsites.net/api/nomikai/search?';
+        let url = 'https://nomikai-funcapp-g4hdgyachmhwfxbq.japaneast-01.azurewebsites.net/api/nomikai/search?';
 
         if (this.searchType === 'date') {
           url += `eventdate=${this.searchDate}`;
@@ -94,7 +94,7 @@ new Vue({
           paymentFlag: result.paymentFlag
         }));
 
-        const response = await fetch('https://m3h-beerkn-functionapp.azurewebsites.net/api/updatepaymentflags', {
+        const response = await fetch('https://nomikai-funcapp-g4hdgyachmhwfxbq.japaneast-01.azurewebsites.net/api/updatepaymentflags', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
